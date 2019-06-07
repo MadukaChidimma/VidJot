bb910c94b453dedebd7d178fa5762988f166e2e4
Maduka Chidimma <maduka.chidimma@gmail.com>
2019-06-07 05:59:09 +0100

    complete

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if('ProcessingInstruction.env.NODE_ENV === production'){
+if(process.env.NODE_ENV === 'production'){
 module.exports ={mongoURI: 'mongodb+srv://Hephzy:Hephzy@cluster0-rtkd2.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
 module.exports = {mongoURI: 'mongodb://localhost:27017/vidjot-dev'}
